6d366e35cf10deea3b24169e218921fd34c15320
george-mountain <georgejiokem@gmail.com>
2023-01-03 11:36:18 +0900

    Added simple test case and installed psycopg2 in our docker file

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+CREATING A SIMPLE FUNCTION
+"""
+
+def add(x, y):
+    """ Add the x and y values and return results """
+    return x + y
+
+def subtract(x, y):
+    """ Subtract x from y and return result """
+    return y - x
