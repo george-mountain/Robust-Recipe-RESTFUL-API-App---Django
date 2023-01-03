73c14e83e9d004111b09853aa4d948f686fc4740
george-mountain <georgejiokem@gmail.com>
2023-01-03 11:54:25 +0900

    modified the test files and calc files

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 CREATING A SIMPLE FUNCTION
 """
 
+
 def add(x, y):
     """ Add the x and y values and return results """
     return x + y
+
 
 def subtract(x, y):
     """ Subtract x from y and return result """
